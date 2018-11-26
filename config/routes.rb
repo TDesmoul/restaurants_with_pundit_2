65ba37606be60cbd4626ebf5091c04fbff3e0734
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2018-11-26 09:46:31 +0100

    initial canvas for course

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
   devise_for :users
-  root to: 'pages#home'
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
