650b7aa4adb2f50c7c69c9128591484e2d1c4a42
Thomas Desmoulins <thomas.desmoulins@gmail.com>
2018-11-26 12:29:38 +0100

    authentication for index too + root to pages#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants
   devise_for :users
-  root to: 'restaurants#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
